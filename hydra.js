bcab79651959e101a462e02d2e27ded0eeab98da
mooijtech <info@mooijtech.com>
2020-05-31 00:22:43 +0200

    Add Electron

---
+++ hydra.js
@@ -0,0 +1,41 @@
+/**
+ * This file is part of go-hydra (https://www.go-hydra.org/)
+ * Copyright (C) 2020 Marten Mooij (https://www.mooijtech.com/)
+ */
+const { app, BrowserWindow, LoadFileOptions } = require('electron')
+
+function createWindow () {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // and load the base.html of the app.
+    win.loadFile('templates/registration/register.html')
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
